b694dcb1daf7e095fd042404bff6c5728a49206b
marina <nicolaimarina@gmail.com>
2021-01-18 09:10:47 -0500

    styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggle(){
     var sec = document.getElementById('sec');
+    var nav = document.getElementById('navigation');
     sec.classList.toggle('active');
+    nav.classList.toggle('active');
 }
 
 toggle();
