f080fb83690f7a917ad08f6f8e7eebc7e9c8befb
marina <nicolaimarina@gmail.com>
2021-01-18 12:00:00 -0500

    update the sun and moon switch

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.querySelector('.sun').addEventListener('click',() => {
-    document.body.classList.add('light')
+    document.body.classList.add('light');
+    document.getElementById("li-sun").style.display = "none";
+    document.getElementById("li-moon").style.display = "initial";
+    
 } )
 
 document.querySelector('.moon').addEventListener('click',() => {
-    document.body.classList.remove('light')
+    document.body.classList.remove('light');
+    document.getElementById("li-sun").style.display = "initial";
+    document.getElementById("li-moon").style.display = "none";
 } )
 
 
-function toggle(){
+document.querySelector('#toggle').addEventListener('click',() => {    
     var sec = document.getElementById('sec');
     var nav = document.getElementById('navigation');
     sec.classList.toggle('active');
     nav.classList.toggle('active');
-}
+})
 
 toggle();
 
